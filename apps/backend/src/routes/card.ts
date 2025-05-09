2d75fc9f4fcf9aac15d6e4173faafa7849882486
pinxhoes <pinxhoes@gmail.com>
2025-05-09 14:34:34 +0200

    remove image route

--- apps/backend/src/routes/card.ts
+++ apps/backend/src/routes/card.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import { checkCardMatch } from '../controllers/card/checkMatch';
-import { revealCardImage } from '../controllers/card/image';
 import { revealCard } from '../controllers/card/reveal';
 
 const router = express.Router();
 
 router.post('/reveal', revealCard);
 router.post('/checkMatch', checkCardMatch);
-router.get('/image', revealCardImage);
 
 export default router;
